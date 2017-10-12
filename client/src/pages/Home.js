4edc180997cccd34ec06d6f8be468dc56e4a4afc
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-10-11 21:21:37 -0500

    added condition to display Play button ONLY for mobile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { FormBtn } from "../components/Form";
 import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
 // import "./Home.css";
 
-const Home = () => 
-  <Wrapper>
-    <Header>
-      <div>Scavenge</div>
-      <div> ~ </div>
-      <div>Or</div>
-      <div> ~ </div>
-      <div>Hunt</div>
-    </Header>
-    <form>
-      <FormBtn
-        className="btn btn-success"
-        role="button"
-        to="/create"
-      >
-        CREATE
-      </FormBtn>
+class Home extends Component {
+  
+  isMobile = () => {
+    if(window.innerWidth<=800 && window.innerHeight<=600){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 
-      <FormBtn
-        className="btn btn-success"
-        role="button"
-        to="/start"
-      >
-        PLAY
-      </FormBtn>
-    </form>
-  </Wrapper>;
+render(){
+  return(
+    <Wrapper>
+      <Header>
+        <div>Scavenge</div>
+        <div> ~ </div>
+        <div>Or</div>
+        <div> ~ </div>
+        <div>Hunt</div>
+      </Header>
+      <form>
+        <FormBtn
+          className="btn btn-success"
+          role="button"
+          to="/create"
+        >
+          CREATE
+        </FormBtn>
+        {
+          this.isMobile() ? (
+            <FormBtn
+              className="btn btn-success"
+              role="button"
+              to="/start"
+            >
+              PLAY
+            </FormBtn>) : "" }
+      </form>
+    </Wrapper>
+  )}
+}
 
 export default Home;
