96736a1a66d4cf7cf141acb90bfb9df800f958b4
unknown <mrashes2@gmail.com>
2017-10-15 20:34:20 -0500

    increased coordinates to 4 digits

--- client/src/pages/PlayGame/PlayGame.js
+++ client/src/pages/PlayGame/PlayGame.js
@@ -74,8 +74,8 @@ class PlayGame extends Component {
         const destLon = this.state.destLon
 
         //makes a range of numbers for the reference
-        const latTruth = destLat-.003<=this.state.currLat<=destLat+.003
-        const lonTruth = destLon-.003<=this.state.currLon<=destLon+.003
+        const latTruth = destLat-.0003<=this.state.currLat<=destLat+.0003
+        const lonTruth = destLon-.0003<=this.state.currLon<=destLon+.0003
 
         //if location comparison correct then display the aframe environment
         if (latTruth && lonTruth) {
@@ -98,8 +98,8 @@ class PlayGame extends Component {
     setCurrLatLon = (lat, lon) =>{
         return new Promise (
             (resolve, reject) => {
-                const currlat = Math.round(1000*lat)/1000;
-                const currlon = Math.round(1000*lon)/1000;
+                const currlat = Math.round(10000*lat)/10000;
+                const currlon = Math.round(10000*lon)/10000;
                 this.setState({
                     currLat: currlat,
                     currLon: currlon
@@ -133,8 +133,8 @@ class PlayGame extends Component {
             const lat = this.state.locations[turn].latitude
             const lon = this.state.locations[turn].longitude
     
-            const destlat = Math.round(1000*lat)/1000;
-            const destlon = Math.round(1000*lon)/1000;
+            const destlat = Math.round(10000*lat)/10000;
+            const destlon = Math.round(10000*lon)/10000;
     
             this.setState({
                 destLat: destlat,
